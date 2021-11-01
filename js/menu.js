171ddae7f08ded7cfd48c390e4d46359c79e7a31
Lucas Palhano Fonseca <lucas_palhano_@hotmail.com>
2021-11-01 15:57:34 -0300

    Ajustes no design e na responsividade

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,22 @@ openMenu.addEventListener("click", function() {
 closeMenu.addEventListener("click", function() {
     responsiveMenu.classList.remove("active-menu");
 });
+
+
+var searchIcon = document.querySelector(".header__search-icon");
+var searchInput = document.querySelector(".header__search-input");
+
+searchIcon.addEventListener("click", function() {
+    searchInput.style.display = "block";
+    searchIcon.classList.remove("header__search-icon");
+    searchIcon.classList.add("header__search-icon-unnactive");
+    searchInput.focus();
+})
+
+searchInput.addEventListener("focusout", function() {
+    if (window.innerWidth > 756) {
+        searchInput.style.display = "none";
+        searchIcon.classList.remove("header__search-icon-unnactive");
+        searchIcon.classList.add("header__search-icon");
+    }
+})
